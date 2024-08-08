113852b9795a20a46089dd479899106e3fb46d1f
momen <momenhassan@gmail.com>
2024-08-08 17:54:32 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './Components/Home/Home.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Layout from './Components/Layout/Layout.jsx'
-import About from './Components/About/About.jsx'
-import Portfolio from './Components/Portfolio/Portfolio.jsx'
-import Contact from './Components/Contact/Contact.jsx'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Home from './Components/Home/Home.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './Components/Layout/Layout.jsx';
+import About from './Components/About/About.jsx';
+import Portfolio from './Components/Portfolio/Portfolio.jsx';
+import Contact from './Components/Contact/Contact.jsx';
 
 function App() {
-
   const myRouter = createBrowserRouter([
     {
       path: '/React-1',
       element: <Layout />,
       children: [
-        { index: true, element: <Home /> }, //Home Route
-        { path: '/About', element: <About /> },
-        { path: '/Portfolio', element: <Portfolio /> },
-        { path: '/Contact', element: <Contact /> },
-        { path: '*', element: <h1 className='text-black'>404 NOT FOUND PAGE</h1> }
-      ]
-    }
+        { index: true, element: <Home /> }, // Home Route
+        { path: 'About', element: <About /> },
+        { path: 'Portfolio', element: <Portfolio /> },
+        { path: 'Contact', element: <Contact /> },
+        { path: '*', element: <h1 className="text-black">404 NOT FOUND PAGE</h1> },
+      ],
+    },
   ]);
 
-  return (
-    <RouterProvider router={myRouter} />
-  )
+  return <RouterProvider router={myRouter} />;
 }
 
 export default App;
+
+
